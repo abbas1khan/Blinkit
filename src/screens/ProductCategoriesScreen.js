8d980ac59d6a85d23153b7b31ec8c4859c950757
Abbas Khan <khanabbas161@gmail.com>
2024-09-01 20:26:11 +0530

    Product category changes

--- src/screens/ProductCategoriesScreen.js
+++ src/screens/ProductCategoriesScreen.js
@@ -1,14 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { colors } from '../utils/Theme'
+import CustomSafeAreaView from '../components/common/CustomSafeAreaView'
 
 const ProductCategoriesScreen = () => {
     return (
-        <View>
-            <Text>ProductCategoriesScreen</Text>
+        <View style={styles.container}>
+            <CustomSafeAreaView>
+                <Text>ProductCategoriesScreen</Text>
+            </CustomSafeAreaView>
         </View>
     )
 }
 
 export default ProductCategoriesScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.white,
+    },
+})  
